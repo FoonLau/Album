4ee0d90a52ec40cef957c0b81ffcacf339b5eeaa
Foon Lau <foonlau@hotmail.com>
2017-05-06 17:46:11 +0800

    add eslint

--- app/reducers/currentScreen.js
+++
@@ -1,9 +0,0 @@
-import * as types from '../actions/types'
-
-export default function currentScreen(state = 'redTab', action) {
-  if (action.type === types.NAVIGATE) {
-    return action.data.nextScreen;
-  } else {
-    return state;
-  }
-}
